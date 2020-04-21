4005958069d8ba90afdbefe39eeea669da8c2db1
tfbm89 <tfbm89@gmail.com>
2020-04-20 20:28:28 -0500

    Proyecto diseño web curso

---
+++ js/main.js
@@ -0,0 +1,32 @@
+console.log("Este es el proyecto");
+
+function mostrar(){
+    document.getElementById('menu').style.display = 'block';
+
+}
+
+function ocultar(){
+    document.getElementById('menu').style.display ='none';
+}
+
+
+function mostrar_ocultar(){
+
+    var caja = document.getElementById("menu");
+
+    if(caja.style.display == "none"){
+        mostrar();
+    }else{
+        ocultar();
+    }
+}
+
+
+function load (){
+    var el = document.getElementById("t");
+    el.addEventListener("click", function(){mostrar_ocultar("four")}, false);  
+}
+
+
+
+
